1943b992863c38facda8f8de5af8eb39070b1997
kaa <artemasmith@gmail.com>
2015-01-09 00:58:25 +0500

    last test case

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source 'https://rubygems.org'
 gem 'rails', '~>4.2.0'
 gem 'slim-rails'
 
+gem 'rails_12factor', group: :production
+
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3'
 gem 'pg'
--- spec/features/trap_spec.rb
+++ spec/features/trap_spec.rb
@@ -20,4 +20,25 @@ describe 'Requests' do
     expect{page.driver.delete('/test', params: { test: '1', hel: '2' })}.to change{Trap.count}.by(1)
     page.driver.status_code.should eql 200
   end
+
+  it 'delete request should be delete method' do
+    page.driver.delete('/test', params: { test: '1', hel: '2' })
+    Trap.last.request_method.should eq 'DELETE'
+  end
+
+  it 'post request should be post method' do
+    page.driver.post('/test', params: { test: '1', hel: '2' })
+    Trap.last.request_method.should eq 'POST'
+  end
+
+  it 'get request should be get method' do
+    page.driver.get('/test', params: { test: '1', hel: '2' })
+    Trap.last.request_method.should eq 'GET'
+  end
+
+  it 'put request should be put method' do
+    page.driver.put('/test', params: { test: '1', hel: '2' })
+    Trap.last.request_method.should eq 'PUT'
+  end
+
 end
